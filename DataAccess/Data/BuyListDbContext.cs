5355dfc9b6fb51766ae17d77660f5aa47cce4218
Artem <artemshadiuk@gmail.com>
2025-03-16 22:37:24 +0100

    Add project files.

---
+++ DataAccess/Data/BuyListDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class BuyListDbContext : DbContext
+    {
+        public BuyListDbContext(DbContextOptions options) : base(options) {  }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.Database.Migrate();
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+}
---
+++ DataAccess/ServiceExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace DataAccess
+{
+    public static class ServiceExtensions
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<BuyListDbContext>(opts => opts.UseNpgsql(connectionString));
+        }
+
+        public static void DataBaseMigrate(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+            var context = serviceProvider.GetRequiredService<BuyListDbContext>();
+            try
+            {
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.Source);
+            }
+        }
+        public static void AddUploadingsFolder(this WebApplication app, string CurrentDirectoryPath)
+        {
+            string imagesDirPath = Path.Combine(CurrentDirectoryPath, app.Configuration["DirImages"]!);
+
+            if (!Directory.Exists(imagesDirPath))
+            {
+                Directory.CreateDirectory(imagesDirPath);
+            }
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(imagesDirPath),
+                RequestPath = "/" + app.Configuration["DirImages"]
+            });
+        }
+    }
+}
