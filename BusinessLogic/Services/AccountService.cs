5f39d5ce65d328c4703514f6d7012ef582b4c984
Artem <artemshadiuk@gmail.com>
2025-03-17 19:11:59 +0100

    Add Account and order services (not implemented)

---
+++ BusinessLogic/Services/AccountService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Entities;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Models.UserModels;
+using DataAccess.Repositories;
+
+namespace BusinessLogic.Services
+{
+    public class AccountService : IAccountService
+    {
+        public readonly IRepository<User> useRepository;
+
+        public AccountService(IRepository<User> useRepository)
+        {
+            this.useRepository = useRepository; 
+        }
+        public Task AddAsync(BaseUserModel createModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLogic/Services/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.DTOs;
+using BusinessLogic.Entities;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Models.OrderModels;
+using DataAccess.Repositories;
+
+namespace BusinessLogic.Services
+{
+    public class OrderService : IOrderService
+    {
+        public readonly IRepository<Order> orderRepository;
+
+        public OrderService(IRepository<Order> orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+        public Task<IEnumerable<OrderDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDto> CreateAsync(BaseOrderModel creationModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
